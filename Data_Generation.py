d329b82b3b92617e79c5e5e5dbd6b96e93356573
Belal Ibrahim <billy.fcih@yahoo.com>
2018-08-16 06:54:48 +0200

    Add grayed data

--- Data_Generation.py
+++ Data_Generation.py
@@ -10,3 +10,5 @@ for img in imgs:
     image = cv2.resize(image, (48, 64))
     cv2.imwrite("Data/Scaled/" + img, image)
 
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite("Data/Grayed/" + img, image)
