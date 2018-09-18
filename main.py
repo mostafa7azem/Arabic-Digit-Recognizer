2ef80e88906e3d9fc6bc53c39e84978d7061a0bc
mostafa7azem <mostafahazem144@yahoo.com>
2018-09-18 11:02:50 +0200

    add knn model instead of CNN

--- main.py
+++ main.py
@@ -9,7 +9,16 @@ from keras.callbacks import ModelCheckpoint
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Dense, Dropout, Flatten
 from keras.models import load_model
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+import keras
+from keras.layers import Conv3D, MaxPool3D, Flatten, Dense, Dropout, Input, BatchNormalization
+from keras.models import Model
+import os
+import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 
 data_path = os.path.join("Data", "BlackMasked")
@@ -32,7 +41,24 @@ np_images = np_images.reshape(-1, 64, 48, 1)
 x_train, x_test, y_train, y_test = train_test_split(np_images, np_digits, test_size=0.1, random_state=0)
 y_train_categorical = to_categorical(y_train, 10)
 y_test_categorical = to_categorical(y_test, 10)
+knn = KNeighborsClassifier(n_neighbors=10)
+dataset_size = len(x_train)
+TwoDim_dataset = x_train.reshape(dataset_size,-2)
+print(TwoDim_dataset,y_train)
+knn.fit(TwoDim_dataset, y_train)
+dataset_size = len(x_test)
+TwoDim_dataset2= x_test.reshape(dataset_size,-2)
+print(knn.score(TwoDim_dataset2, y_test))
+print("test dataset : values =")
+print(y_test)
+print("predicted  : values =")
+print(knn.predict(TwoDim_dataset2))
+for i in range(len(x_test)):
+    cv2.imshow("Actual: " + str(y_test[i]) + " Predicted: " + str(knn.predict(TwoDim_dataset2)[i]), x_test[i])
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
+'''
 model = Sequential()
 model.add(Conv2D(64, 7, activation='relu', input_shape=(64, 48, 1)))
 model.add(MaxPooling2D(2))
@@ -53,17 +79,20 @@ model.summary()
 model.compile(optimizer=Adam(lr=0.0001), loss='categorical_crossentropy', metrics=["accuracy"])
 model.fit(x_train, y_train_categorical, epochs=7, validation_split=0.1, callbacks=[checkpoint, early_stop])
 model.save('model.h5')
-# Load the model
-model = load_model('model.h5')
 
+model = load_model('model.h5')
+[node.op.name for node in model.outputs]
+print(model.summary())
 predictions = model.predict_classes(x_test)
 loss, accuracy = model.evaluate(x_test, y_test_categorical)
 print(predictions)
 print(y_test)
 print("Loss: " + str(loss))
 print("Accuracy (%): " + str(accuracy*100))
-
+'''
+'''
 for i in range(len(x_test)):
     cv2.imshow("Actual: " + str(y_test[i]) + " Predicted: " + str(predictions[i]), x_test[i])
     cv2.waitKey()
     cv2.destroyAllWindows()
+'''
